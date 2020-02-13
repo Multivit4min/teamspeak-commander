255a795899a88b162a3493620cbc19bc39def0d2
multivit4min <david.kartnaller@gmail.com>
2020-02-13 12:56:04 +0100

    add more exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export { Commander } from "./Commander"
+export { Commander, CommanderOptions } from "./Commander"
 export { Command } from "./command/Command"
 export { CommandGroup } from "./command/CommandGroup"
+export { BaseCommand } from "./command/BaseCommand"
 export {
   CommanderTextMessage,
   BaseTranslationProps,
